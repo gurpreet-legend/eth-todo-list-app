a9625f504a200b1f202e20bd538b29cf3627fd27
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2022-02-12 06:13:25 +0530

    minor changes

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,39 +1,44 @@
 import Task from "../Task/Task";
 
-const TaskList = ({tasks, account, methods}) => {
+const TaskList = ({ tasks, account, methods }) => {
 
     const toggleCompleted = async (taskId) => {
-        const result = await methods.toggleCompleted(parseInt(taskId)).send({from: account});
+        const result = await methods.toggleCompleted(parseInt(taskId)).send({ from: account });
         console.log(result);
+        document.location.reload();
     }
     return (
         <fieldset className="space-y-6">
             <legend className="text-xl font-bold text-indigo-500">Todo Tasks</legend>
-            {
-                tasks.map((task, index) => {
-                    if(task.completed == false){
-                        return (
-                            <div key={index} className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
-                                <Task toggleCompleted={toggleCompleted} methods={methods} account={account} taskContent={task.content} taskNumber={task.id} completed={task.completed}/>
-                            </div>
-                        )
-                    }
-                })
-            }
-            <hr/>
+            <div className="grid gap-y-6 grid-cols-1 sm:grid-cols-3 sm:gap-x-4">
+                {
+                    tasks.map((task, index) => {
+                        if (task.completed == false) {
+                            return (
+                                <div key={index} className="mt-4 col-span-1">
+                                    <Task toggleCompleted={toggleCompleted} methods={methods} account={account} taskContent={task.content} taskNumber={task.id} completed={task.completed} />
+                                </div>
+                            )
+                        }
+                    })
+                }
+            </div>
+            <hr />
             <legend className="text-xl font-bold text-indigo-500">Completed Tasks</legend>
-            {
-                tasks.map((task, index) => {
-                    if(task.completed == true){
-                        return (
-                            <div key={index} className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
-                                <Task toggleCompleted={toggleCompleted} methods={methods} aacount={account} taskContent={task.content} taskNumber={task.id} completed={task.completed}/>
-                            </div>
+            <div className="grid gap-y-6 grid-cols-1 sm:grid-cols-3 sm:gap-x-4">
+                {
+                    tasks.map((task, index) => {
+                        if (task.completed == true) {
+                            return (
+                                <div key={index} className="mt-4 col-span-1">
+                                    <Task toggleCompleted={toggleCompleted} methods={methods} aacount={account} taskContent={task.content} taskNumber={task.id} completed={task.completed} />
+                                </div>
 
-                        )
-                    }
-                })
-            }
+                            )
+                        }
+                    })
+                }
+            </div>
         </fieldset>
 
     )
